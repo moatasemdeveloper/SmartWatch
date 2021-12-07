67fa89aabccd0a8b4caaf0dba9fc53d26ba173c9
Ahmed-Zein-elsayed <zein279021@gmail.com>
2021-12-07 12:22:36 +0200

    LED Driver

---
+++ Led.cpp
@@ -0,0 +1,20 @@
+#include "Led.h"
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+LED::LED()
+{
+	DDRB = 0xff;
+	DDRA=0xff;
+	PORTB = 0x00;
+	PORTA=0x00;
+}
+void LED::LED_toggle()
+{
+	_delay_ms(500);
+	PORTB = ~PORTB;
+	_delay_ms(500);
+	PORTA = ~PORTA;
+        
+}
